fb431c84c4f77f0a06498095f74d7a1ddc6ea6dd
Boris Kaiser <mail@boriskaiser.com>
2015-09-26 21:29:23 +0200

    Add missing semicolon

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
-var through2 = require('through2')
+var through2 = require('through2');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 
