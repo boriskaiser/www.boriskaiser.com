354b2dfb0a68c434b4e5eab2956c92126c46c0cb
Boris Kaiser <mail@boriskaiser.com>
2015-09-28 21:20:07 +0200

    Move current page to React

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log("Let's rock ...");
+var React = require('React');
+
+var Logo = require('./logo');
+
+var goldColor = '#e4bc82';
+var stageStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    boxShadow: 'inset 0 0 0 5px ' + goldColor
+};
+
+var Stage = React.createClass({
+    render: function () {
+        return (
+            <div className="stage" style={stageStyle}>
+                <a href="mailto:mail@boriskaiser.com">
+                    <Logo fill={goldColor}/>
+                </a>
+            </div>
+        );
+    }
+});
+
+window.addEventListener('load', function () {
+    document.body.style.margin = 0;
+
+    React.render(
+        <Stage/>,
+        document.body
+    );
+});
